d32b59d3694cd59aa3c35db55a9defd2c87863e7
Nishizaki <steelpipe75@gmail.com>
2013-11-25 23:50:11 +0900

    リリース用のファイルコピーをbat化

---
+++ release.bat
@@ -0,0 +1,6 @@
+copy Barlog.exe .\Barlog\Barlog.exe /Y
+copy convert.yaml .\Barlog\convert.yaml /Y
+copy MIT-LICENSE.txt .\Barlog\MIT-LICENSE.txt /Y
+copy README.md .\Barlog\README.md /Y
+copy Barlog.rb .\Barlog\Barlog.rb /Y
+copy input.csv .\Barlog\input.csv
